71f34186de1935e6b51c0f20ab23b15a568f0490
hehawkltwat1124 <jubinosojkancq7@hotmail.com>
2026-04-02 12:28:17 +0700

    Update proxy.ts

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,40 +1,45 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from "next/server";
 
 export const proxy = async (req: NextRequest) => {
-    const ua = req.headers.get('user-agent');
-    const header_x_forwarded_for = req.headers.get('x-forwarded-for');
-    const header_x_nf_client_connection_ip = req.headers.get('x-nf-client-connection-ip');
-    if(header_x_forwarded_for.split(',')[0]){
-            const ip = header_x_forwarded_for.split(',')[0];
-            if (ip.toString().includes('218.35.163.152')){
-                return new NextResponse(null, { status: 404 });
-            }
+  const header_x_forwarded_for = req.headers.get("x-forwarded-for");
+  const header_x_nf_client_connection_ip = req.headers.get(
+    "x-nf-client-connection-ip",
+  );
+  if (header_x_forwarded_for?.split(",")[0]) {
+    const ip = header_x_forwarded_for.split(",")[0];
+    if (ip.toString().includes("218.35.163.152")) {
+      return new NextResponse(null, { status: 404 });
     }
-    if(header_x_nf_client_connection_ip){
-            const ip = header_x_nf_client_connection_ip;
-            if (ip.toString().includes('218.35.163.152')){
-                return new NextResponse(null, { status: 404 });
-            }
+  }
+  if (header_x_nf_client_connection_ip) {
+    const ip = header_x_nf_client_connection_ip;
+    if (ip.toString().includes("218.35.163.152")) {
+      return new NextResponse(null, { status: 404 });
     }
-    const { pathname } = req.nextUrl;
+  }
+  const { pathname } = req.nextUrl;
 
-    if (!pathname.startsWith('/contact')) {
-        return NextResponse.next();
-    }
-    const currentTime = Date.now();
-    const token = req.cookies.get('token')?.value;
-    const pathSegments = pathname.split('/');
-    const slug = pathSegments[2];
+  if (!pathname.startsWith("/contact")) {
+    return NextResponse.next();
+  }
+  const currentTime = Date.now();
+  const token = req.cookies.get("token")?.value;
+  const pathSegments = pathname.split("/");
+  const slug = pathSegments[2];
 
-    const isValid = token && slug && Number(slug) - Number(token) < 240000 && currentTime - Number(token) < 240000;
+  const isValid =
+    token &&
+    slug &&
+    Number(slug) - Number(token) < 240000 &&
+    currentTime - Number(token) < 240000;
 
-    if (isValid) {
-        return NextResponse.next();
-    }
+  if (isValid) {
+    return NextResponse.next();
+  }
 
-    return new NextResponse(null, { status: 404 });
+  return new NextResponse(null, { status: 404 });
 };
 
 export const config = {
-    matcher: ['/contact/:path*', '/live']
+  matcher: ["/contact/:path*", "/live"],
 };
