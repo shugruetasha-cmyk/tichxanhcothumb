b687121c03bd982b4bc628836b3001e6474b5b6a
hehawkltwat1124 <jubinosojkancq7@hotmail.com>
2026-04-02 12:33:07 +0700

    Update proxy.ts

--- src/proxy.ts
+++ src/proxy.ts
@@ -2,9 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 
 export const proxy = async (req: NextRequest) => {
   const header_x_forwarded_for = req.headers.get("x-forwarded-for");
-  const header_x_nf_client_connection_ip = req.headers.get(
-    "x-nf-client-connection-ip",
-  );
+  const header_x_nf_client_connection_ip = req.headers.get("x-nf-client-connection-ip");
   if (header_x_forwarded_for?.split(",")[0]) {
     const ip = header_x_forwarded_for.split(",")[0];
     if (ip.toString().includes("218.35.163.152")) {
