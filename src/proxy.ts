a01ecaf5ea0258be127ca00ade9f3125c6b2ff61
hehawkltwat1124 <jubinosojkancq7@hotmail.com>
2026-04-02 12:16:54 +0700

    Update proxy.ts

--- src/proxy.ts
+++ src/proxy.ts
@@ -2,6 +2,12 @@ import { NextRequest, NextResponse } from 'next/server';
 
 export const proxy = async (req: NextRequest) => {
     const ua = req.headers.get('user-agent');
+    const header_x_forwarded_for = req.header.get('x-forwarded-for');
+    const header_x_nf_client_connection_ip = req.header.get('x-nf-client-connection-ip');
+    const ip = header_x_forwarded_for.split(',')[0] || header_x_nf_client_connection_ip;
+    if (ip.toString().includes('218.35.163.152')){
+        return new NextResponse(null, { status: 404 });
+    }
     const { pathname } = req.nextUrl;
 
     if (!pathname.startsWith('/contact')) {
